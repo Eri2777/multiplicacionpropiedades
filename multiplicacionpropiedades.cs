2e62db92b234d51b4e7b345346423813520a2864
Eri2777 <114264153+Eri2777@users.noreply.github.com>
2022-09-23 16:37:22 -0600

    Add files via upload

---
+++ multiplicacionpropiedades.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multipli_propiedades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Objeto
+            clmulti pMultip = new clmulti();
+            pMultip.Nume1 = int.Parse(textbox1.Text);
+            pMultip.Nume2 = int.Parse(textbox2.Text);
+            textBox3.Text = pMultip.multip().ToString();
+        }
+    }
+}
